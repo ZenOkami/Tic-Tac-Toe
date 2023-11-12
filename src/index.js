595398e29cc894b89375a22bda2197d5bee19785
ZenOkami <gianstasi@yahoo.com>
2023-11-12 15:02:50 -0700

    Part 2 Commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,46 @@
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
-playBoard = document.querySelector(".play-board");
-
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll('section span'),
+players = document.querySelector('.players');
 
 // Start Window
-function initGame() {
-    console.log('Button clicked');
-    selectBox.classList.add("hide");
-    playBoard.classList.add("show");
-}
-
 window.onload = () => {
-    console.log("window loaded");
-    selectXBtn.addEventListener('click', initGame);
-    selectOBtn.addEventListener('click', initGame);
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].addEventListener("click", () => {
+            clickedBox(allBox[i]);
+        }, false);
+    }
+    
+    selectXBtn.addEventListener('click', () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+    });
+    selectOBtn.addEventListener('click', () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+        players.setAttribute('class', 'players active player');
+    });
 };
 
-// Next Steps
+// Player Icon and Sign
+let playerXIcon = 'fas fa-times';
+let playerOIcon = 'far fa-circle'
+let playerSign = 'X';
+let runBot = true;
+
+// Clicked Box Function
+function clickedBox(element) {
+    if (players.classList.contains("player")) {
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.add('active');
+        element.setAttribute("id", playerSign)
+    } else {
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        players.classList.add('active');
+        element.setAttribute("id", playerSign)
+    }
+    playBoard.computedStyleMap.pointerEvents = "none";
+    element.style.pointerEvents = "none";
+};
